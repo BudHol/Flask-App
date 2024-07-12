4461dc34cd76d67cfc6ce1276001b9af2990f52d
Abbad <abbadshahabi01@gmail.com>
2024-07-13 01:54:27 +0500

    Commit 3

--- app/routes.py
+++ app/routes.py
@@ -3,5 +3,16 @@ from app import app
 @app.route('/') 
 @app.route('/index') 
 def index(): 
-    user = {"username": "Gopal"}
-    return render_template('index.html', title = 'Home', user=user)
+    user = {"username": "Gopal"} 
+    posts = [
+        {
+            "author":{"username":"Mikel"},
+            "body":"Hi, the weather is great here in Portland!!"
+        }, 
+        {
+            "author":{"username":"Gege"},
+            "body":"Nah, imma bring back Kenny once more"
+        }
+        
+    ]
+    return render_template('index.html', title = 'Home', user=user, posts=posts)
